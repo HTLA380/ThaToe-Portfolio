4589d2537feeda807e87bd9e6670795f8122e855
Htet Aung Lin <htetlin099798@gmail.com>
2024-05-22 22:18:51 +0630

    feat: add navbar

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import classNames from 'classnames';
+
+const navLink = [
+  { name: 'home', route: '/' },
+  { name: 'film', route: '/film' },
+  { name: 'photography', route: '/photography' },
+  { name: 'blog', route: '/blog' },
+  { name: 'about', route: '/about' },
+  { name: 'contact', route: '/contact' },
+  { name: 'newsletter', route: '/newsletter' },
+];
+
+const Navbar = () => {
+  const pathname = usePathname();
+
+  return (
+    <header className='h-138px w-full px-16 flex items-center justify-center bg-white_color/85'>
+      <div className='w-full max-w-4xl flex items-center justify-between'>
+        <Image
+          className='ml-4'
+          src={'/assets/images/logo.png'}
+          width={170}
+          height={128}
+          alt='art by tha-toe'
+          priority={false}
+        />
+
+        <nav>
+          <ul className='flex items-center'>
+            {navLink.map((item, index) => (
+              <li key={index}>
+                <Link
+                  className={classNames('uppercase px-10px transition-colors duration-500 hover:text-yellow_color', {
+                    'text-cyan_color': item.route === pathname,
+                  })}
+                  href={item.route}>
+                  {item.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
